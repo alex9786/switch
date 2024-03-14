8d7b8be0656d4f5115267fa1e2404c73c4e9f538
alex9786 <alexdce007@gmail.com>
2024-03-14 12:28:07 +0530

    first commit

---
+++ example.pro.java
@@ -0,0 +1,34 @@
+
+package world;
+
+public class switchs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int day=5;
+		switch (day){
+		case 1:
+			System.out.println(" anowalday");
+		break;
+		case 2 :
+			System.out.println(" workingday");
+		  break;
+		case 3:
+		     System.out.println(" holyday");	
+		break;
+		default:
+			System.out.println(" nothing in schedule");
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+}
